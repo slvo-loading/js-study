e340a4eb78ce90502f6e5b1c41393ddb6e2ad7aa
slvo-loading <sydneyvo098@gmail.com>
2024-07-23 23:33:55 -0700

    fixed a bug in currency

--- currency.js
+++ currency.js
@@ -1,9 +1,13 @@
-let yuan = Number(prompt("What do you have left in yuan? "))
-let yen = Number(prompt("What do you have left in yen? "))
-let won = Number(prompt("What do you have left in won? "))
+let yuan = 560
+let yen = 2455
+let won = 3280
 
-yuan *= 7.27
-yen *= 154.45
-won *= 1383.46
+console.log("What do you have left in yuan? " + yuan)
+console.log("What do you have left in yen? " + yen)
+console.log("What do you have left in won? " + won)
+
+yuan *= 0.14
+yen *= 0.0065
+won *= 0.00072
 
 console.log(yuan + yen + won)
