df7fe49ca8405abab7118966fe21808ca97393a0
slvo-loading <sydneyvo098@gmail.com>
2024-08-05 21:30:13 -0700

    finished js in codedex

--- cap_that/script.js
+++ cap_that/script.js
@@ -5,4 +5,16 @@ const memeArray = [
     "https://i.imgur.com/Qqoxh1N.png"
   ];
 
-  
+
+  captionsArray = ["the one piece is real!", "that's when I realized, I f*cked up", "scibbidi", "sigma"];
+
+  const img = document.getElementById("random_meme");
+  const caption = document.getElementById("random-caption");
+  const button = document.getElementById("generator-button");
+
+  button.addEventListener("click", function(){
+    const idx_meme = Math.floor(Math.random() * memeArray.length);
+    const idx_caption = Math.floor(Math.random() * captionsArray.length);
+    img.src = memeArray[idx_meme];
+    caption.innerText = captionsArray[idx_caption]
+  });
